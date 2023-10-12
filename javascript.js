a5becc8435faf4e03337866482449a9d7b1dc8d0
purple1violet <143123312+purple1violet@users.noreply.github.com>
2023-10-11 22:02:36 -0400

    concise the code

--- javascript.js
+++ javascript.js
@@ -1,12 +1,6 @@
-const canvasGrid = document.createElement("div");
-canvasGrid.classList.add("canvasGrid")
-const canvasContainer = document.querySelector("#canvasContainer")
-
 let repeatTimes= 16*16;
 
-
 let createGrid = function(){
-
     for (let i = 0; i < repeatTimes; i++){
         const canvasGrid = document.createElement("div");
         canvasGrid.classList.add("canvasGrid");
